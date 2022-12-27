5b069ae10579e0209f60aea3d5f61ff76d6362ab
catkidd <catkidd770@gmail.com>
2022-12-27 11:36:12 +0545

    To find the greatest among three variables

---
+++ greatest.py
@@ -0,0 +1,12 @@
+# To find the greatest among three variables
+
+a = 10
+b = 20
+c = 15
+
+if a > b and a > c:
+    print("a is greatest.")
+elif b > c:
+    print("b is greatest.")
+else:
+    print("c is greatest.")
