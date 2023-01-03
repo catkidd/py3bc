bffac74bc3661b00a252b9a6d12c5f2fc128a249
catkidd <catkidd770@gmail.com>
2023-01-03 10:37:26 +0545

    string operations

---
+++ stringop.py
@@ -0,0 +1,18 @@
+st = "python is fun programming language."
+print(st)
+# capitalize the sentence
+print(st.capitalize())
+# capitalize each word
+print(st.title())
+# change to all caps
+st = st.upper()
+print(st)
+# change to all low
+st = st.lower()
+print(st)
+# split the sentences using common literal
+stlist = st.split(" ")
+print(stlist)
+# to replace something in the string
+st = st.replace("fun", "funny")
+print(st)
