e9391c2229d755dcf93a07bb0caae4f550efb243
catkidd <catkidd770@gmail.com>
2023-01-03 10:43:17 +0545

    updated code

--- list.py
+++ list.py
@@ -1,22 +1,16 @@
 # Write a program to store first 10 even number and first 10 odd number in a list and print their sum.
 
-elist = []
-olist = []
-esum = 0
-osum = 0
+eolist = []
+sum = 0
+
 
 for i in range(1, 21):
     if i % 2:
-        olist.append(i)
-        osum += i
+        eolist.append(i)
+    else:
+        eolist.append(i)
+    sum += i
 
-for j in range(1, 21):
-    if j % 2 == 0:
-        elist.append(j)
-        esum += j
 
-print(olist)
-print(osum)
-print(elist)
-print(esum)
-print(esum + osum)
+print(eolist)
+print(f"The sum of first 10 even and first 10 odd number is: {sum} ")
