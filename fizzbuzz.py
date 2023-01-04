157d5bb20dd5abc1be6040478b59a2ae06b81ffc
catkidd <catkidd770@gmail.com>
2023-01-04 12:54:26 +0545

    new appraoch added

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,9 +3,25 @@ where n is the input provided to the program. But for multiples of three print "
 and for the multiples of five print "Buzz". 
 For numbers which are multiples of both three and five print "FizzBuzz"."""
 
-# approach 1 -->
-def fizzbuzz():
-    for i in range(1, 101):
+# approach 1 --> range 1 - 100
+# def fizzbuzz():
+#     for i in range(1, 101):
+#         # if i % 3 == 0 and i % 5 == 0:
+#         if i % 15 == 0:
+#             print("FizzBuzz")
+#         elif i % 5 == 0:
+#             print("Buzz")
+#         elif i % 3 == 0:
+#             print("Fizz")
+#         else:
+#             print(i)
+
+
+# fizzbuzz()
+
+# approach 2 --> range - user input
+def fizzbuzz(n):
+    for i in range(1, n+1):
         # if i % 3 == 0 and i % 5 == 0:
         if i % 15 == 0:
             print("FizzBuzz")
@@ -17,4 +33,10 @@ def fizzbuzz():
             print(i)
 
 
-fizzbuzz()
+while True:
+    n = int(input("Enter any range greater than 50 : "))
+
+    if n >= 50:
+        break
+
+fizzbuzz(n)
